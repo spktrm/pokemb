542332cb0b912159efaef1d0e21f1812541901f1
spktrm <joseph.twin14@gmail.com>
2023-12-16 08:01:56 +1000

    update

--- pokemb/mod.py
+++ pokemb/mod.py
@@ -47,9 +47,11 @@ class ComponentEmbedding(nn.Module):
         self,
         gendata: Dict[str, Any],
         name: str,
-        output_size: int,
+        num_layers: int = 1,
         num_unknown: int = 1,
         use_layer_norm: bool = False,
+        include_mlp: bool = False,
+        output_size: int = None,
     ):
         super().__init__()
         self.name = name
@@ -67,17 +69,23 @@ class ComponentEmbedding(nn.Module):
         self.unknown = nn.Embedding(num_unknown, output_size)
         self.data = nn.Embedding.from_pretrained(torch.from_numpy(placeholder).float())
 
-        self.mlp = make_mlp(
-            vector_size, output_size, n_layers=3, layer_norm=use_layer_norm
-        )
+        self.include_mlp = include_mlp
+        if self.include_mlp:
+            self.mlp = make_mlp(
+                vector_size, output_size, n_layers=num_layers, layer_norm=use_layer_norm
+            )
         self.names = data["names"].tolist()
 
+    def forward_indices(self, indices: torch.Tensor):
+        return self.data((indices - self.num_unknown).clamp(min=0))
+
     def forward(self, indices: torch.Tensor):
-        return torch.where(
-            (indices < self.num_unknown).unsqueeze(-1),
-            self.unknown(indices.clamp(min=0, max=self.num_unknown - 1)),
-            self.mlp(self.data((indices - self.num_unknown).clamp(min=0))),
-        )
+        if self.include_mlp:
+            return torch.where(
+                (indices < self.num_unknown).unsqueeze(-1),
+                self.unknown(indices.clamp(min=0, max=self.num_unknown - 1)),
+                self.mlp(self.data((indices - self.num_unknown).clamp(min=0))),
+            )
 
 
 class PokEmb(nn.Module):
@@ -86,6 +94,7 @@ class PokEmb(nn.Module):
         gen: int = 9,
         output_size: EncodingSize = None,
         use_layer_norm: bool = False,
+        include_mlp: bool = False,
     ):
         super().__init__()
 
@@ -94,44 +103,34 @@ class PokEmb(nn.Module):
         self.species = ComponentEmbedding(
             gendata=gendata,
             name="species",
-            output_size=output_size,
-            num_unknown=1,
+            num_unknown=2,
             use_layer_norm=use_layer_norm,
+            include_mlp=include_mlp,
+            output_size=output_size,
         )
         self.moves = ComponentEmbedding(
             gendata=gendata,
             name="moves",
-            output_size=output_size,
-            num_unknown=2,
+            num_unknown=3,
             use_layer_norm=use_layer_norm,
+            include_mlp=include_mlp,
+            output_size=output_size,
         )
         self.abilities = ComponentEmbedding(
             gendata=gendata,
             name="abilities",
-            output_size=output_size,
-            num_unknown=1,
+            num_unknown=2,
             use_layer_norm=use_layer_norm,
+            include_mlp=include_mlp,
+            output_size=output_size,
         )
         self.items = ComponentEmbedding(
             gendata=gendata,
             name="items",
-            output_size=output_size,
-            num_unknown=1,
+            num_unknown=2,
             use_layer_norm=use_layer_norm,
-        )
-
-    def forward(
-        self,
-        species_indices: torch.Tensor,
-        moves_indices: torch.Tensor,
-        abilities_indices: torch.Tensor,
-        items_indices: torch.Tensor,
-    ) -> torch.Tensor:
-        return (
-            F.normalize(self.species(species_indices), dim=-1)
-            + F.normalize(self.moves(moves_indices).sum(-2), dim=-1)
-            + F.normalize(self.abilities(abilities_indices), dim=-1)
-            + F.normalize(self.items(items_indices), dim=-1)
+            include_mlp=include_mlp,
+            output_size=output_size,
         )
 
     def forward_species(self, indices: torch.Tensor) -> torch.Tensor:
